017bfa41c6af4c224aad31fde82b9813dbc537ea
only1design <kyrytin@gmail.com>
2026-03-03 14:19:49 +0100

    feat: implement interactive function

--- src/cli/interactive.js
+++ src/cli/interactive.js
@@ -1,8 +1,40 @@
+import readline from "node:readline";
+import util from "node:util";
+
+const UNKNOWN_COMMAND = 'Unknown command'
+const SEPARATOR = '---=======---'
+
 const interactive = () => {
-  // Write your code here
-  // Use readline module for interactive CLI
-  // Support commands: uptime, cwd, date, exit
-  // Handle Ctrl+C and unknown commands
+    const commands = {
+        uptime: () => console.log(`Uptime: ${process.uptime().toFixed(2)}s`),
+        cwd: () => console.log(process.cwd()),
+        date: () => console.log(new Date().toISOString()),
+        exit: (rl) => rl.close()
+    }
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: '> ',
+    })
+
+    rl.on('line', async (line) => {
+        const commandName = line.trim();
+        const handleCommand = commands[commandName];
+
+        if (handleCommand) {
+            handleCommand(rl);
+        } else {
+            console.log(util.styleText('red', UNKNOWN_COMMAND))
+        }
+
+        if (handleCommand !== commands.exit) {
+            console.log(util.styleText('dim', SEPARATOR))
+        }
+    });
+    rl.on('close', () => {
+        console.log('Goodbye!')
+    })
 };
 
 interactive();
