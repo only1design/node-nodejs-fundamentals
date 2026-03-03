9506bad6099e5a2fad68fa0585e4ca2137fb78db
only1design <kyrytin@gmail.com>
2026-03-03 22:02:08 +0100

    feat: implement dynamic function

--- src/modules/dynamic.js
+++ src/modules/dynamic.js
@@ -1,9 +1,33 @@
+import util from "node:util";
+import path from "node:path";
+import {fileURLToPath} from "node:url";
+import fs from "node:fs";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const pluginsPath = path.join(__dirname, 'plugins');
+
+const exit = () => {
+    console.log('Plugin not found');
+    process.exit(1);
+}
+
 const dynamic = async () => {
-  // Write your code here
-  // Accept plugin name as CLI argument
-  // Dynamically import plugin from plugins/ directory
-  // Call run() function and print result
-  // Handle missing plugin case
+    const command = process.argv[2];
+
+    if (!command) exit();
+
+    const pluginPath = path.join(pluginsPath,  `${command}.js`);
+
+    try {
+        await fs.promises.access(pluginPath);
+    } catch (err) {
+        exit()
+    }
+
+    const {run} = await import(pluginPath)
+    const result = await run();
+
+    console.log(result)
 };
 
 await dynamic();
